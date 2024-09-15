8a5cf562ce00f3f201747b6305bdcbed7982a2e6
Jessica Kincaid <4j.kincaid@gmail.com>
2024-09-15 16:58:40 -0500

    edited animation to improve user experience

--- script.js
+++ script.js
@@ -5,28 +5,28 @@
 
 $(function() {
     $("#filter0").click(function() {
-    $('#books li:first').animate({ width: "150px" }, 1000);
+    $('#books li:first').animate({ width: "150px", height: "130px" }, 1000);
     });
 });
 $(function() {
     $("#filter1").click(function() {
-    $('#books li:eq(1)').animate({ width: "150px" }, 1000);
+    $('#books li:eq(1)').animate({ width: "150px", height: "130px" }, 1000);
     });
 });
 $(function() {
     $("#filter2").click(function() {
-    $('#books li:eq(2)').animate({ width: "150px" }, 1000);
+    $('#books li:eq(2)').animate({ width: "150px", height: "130px" }, 1000);
     });
 });
 $(function() {
     $("#filter4").click(function() {
-    $('#books li:eq(3)').animate({ width: "150px" }, 1000);
+    $('#books li:eq(3)').animate({ width: "150px", height: "130px" }, 1000);
     });
 });
 // This one widens the 4th book when you hit the Filter button:
     $(function() {
         $("#filter5").click(function() {
-        $('#books li:last').animate({ width: "150px" }, 1000);
+        $('#books li:last').animate({ width: "150px", height: "130px" }, 1000);
         });
     });
 
