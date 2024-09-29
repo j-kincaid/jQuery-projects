d70d05d99c694047fc6dde164137af91a0bd4284
Jessica Kincaid <4j.kincaid@gmail.com>
2024-09-29 14:00:45 -0500

    add navigation

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
-// To use a filter to apply styles to specific items in the ordered list
-
-// These widen the book when you hit the Filter button:
+// Book covers are 600px x 400px
 
 $(function() {
     $("#filter0").click(function() {
