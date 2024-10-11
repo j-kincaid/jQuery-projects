67db2bdfd4f2ebf8a6ac604302bf2a0cd30e16f2
Jessica Kincaid <4j.kincaid@gmail.com>
2024-10-11 16:53:10 -0500

    buttons are making the covers slide to left and right but they still need different starting positions

--- script.js
+++ script.js
@@ -3,32 +3,51 @@
 
 // These widen the book when you hit the Filter button:
 
+// $(function(){
+//     $('#book1').mouseenter(function(){
+//       $("li").animate({right: '250px'});
+//     });
+//   });
+
+// $(function(){
+//     $("book1").mouseenter(function(){
+//         $("li").animate({left: '250px'});
+//     });
+// });
+
 $(function() {
-    $("#filter0").click(function() {
-    $('#books li:first').animate({ width: "150px", height: "130px" }, 1000);
+    $(".filter0").click(function() {
+    $('#books li:first').animate({ left: '10px' }, 1000);
     });
 });
+
 $(function() {
     $("#filter1").click(function() {
-    $('#books li:eq(1)').animate({ width: "150px", height: "130px" }, 1000);
+    $('#books li:eq(1)').animate({ right: '1px' }, 1000);
     });
 });
 $(function() {
     $("#filter2").click(function() {
-    $('#books li:eq(2)').animate({ width: "150px", height: "130px" }, 1000);
+    $('#books li:eq(2)').animate({ left: '-2px' }, 1000);
     });
 });
 $(function() {
     $("#filter4").click(function() {
-    $('#books li:eq(3)').animate({ width: "150px", height: "130px" }, 1000);
+    $('#books li:eq(3)').animate({ left: '300px' }, 1000);
     });
 });
 // This one widens the 4th book when you hit the Filter button:
-    $(function() {
-        $("#filter5").click(function() {
-        $('#books li:last').animate({ width: "150px", height: "130px" }, 1000);
-        });
+$(document).ready(function(){
+    $("button").click(function(){
+      $("div").animate({left: '250px'});
     });
+  });
+
+$(function() {
+    $("#filter5").click(function() {
+    $('#books li:last').animate({right: '1em'});
+    });
+});
 
 
 
